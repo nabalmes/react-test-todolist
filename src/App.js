a8a974faae446bbf69c947cbdd31c3cede5652bd
Neil Balmes <nabalmes@integranet.ph>
2023-01-30 09:04:44 +0800

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { useState, useRef}from 'react';
+import React, { useState, useRef, useEffect}from 'react';
 import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
  const [todos, setTodos] = useState([])
  const todoNameRef = useRef()
+ 
+ useEffect(()=> {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(todos)))
+  if(storedTodos) setTodos( prevTodos => [...prevTodos, ...storedTodos] );
+ }, [])
+
+ useEffect(()=>{
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+ }, [todos])
+
+
 
  function handleAddTodo(e) {
   const name =  todoNameRef.current.value
   if (name === '') return
-    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
   
  }
   return (
